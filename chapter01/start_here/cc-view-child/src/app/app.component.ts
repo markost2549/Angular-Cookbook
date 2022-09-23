99652045da3831f3bb9fb43d6d49982012b0c118
Marko Stojkovic <markost2549@gmail.com>
2022-09-23 21:32:24 +0200

    access component by @viewChild

--- chapter01/start_here/cc-view-child/src/app/app.component.ts
+++ chapter01/start_here/cc-view-child/src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'cc-view-child';
+  @ViewChild(GalleryComponent) gallery;
 
   addNewPicture() {
+    this.gallery.pictures.unshift(this.gallery.generateImage());
     console.log('added new picture');
   }
 
   removeFirstPicture() {
+    this.gallery.pictures.shift();
     console.log('removed first picture');
   }
 }
