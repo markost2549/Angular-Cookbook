b81f9a0f9a0683667b13aed1bddd8828abb664e4
Marko Stojkovic <markost2549@gmail.com>
2022-09-23 21:00:39 +0200

    getter and setter

--- chapter01/start_here/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
+++ chapter01/start_here/cc-setters/src/app/components/vc-logs/vc-logs.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-vc-logs',
   templateUrl: './vc-logs.component.html',
-  styleUrls: ['./vc-logs.component.scss']
+  styleUrls: ['./vc-logs.component.scss'],
 })
 export class VcLogsComponent implements OnInit {
-  @Input() vName;
-  constructor() { }
-
-  ngOnInit(): void {
+  _vName: string;
+  @Input()
+  get vName() {
+    return this._vName;
   }
+  set vName(name: string) {
+    if (!name) return;
+    if (!this._vName) {
+      this.logs.push(`initial version is ${name.trim()}`);
+    } else {
+      this.logs.push(`version changed to ${name.trim()}`);
+    }
+    this._vName = name;
+  }
+  logs: String[] = [];
+  constructor() {}
 
+  ngOnInit(): void {}
 }
