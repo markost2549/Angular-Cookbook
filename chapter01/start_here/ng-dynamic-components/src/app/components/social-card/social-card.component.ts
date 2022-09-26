fd5ac9694949af57e1259f088de33933a6d3ca72
Marko Stojkovic <markost2549@gmail.com>
2022-09-26 23:19:18 +0200

    highlight text

--- chapter01/start_here/ng-dynamic-components/src/app/components/social-card/social-card.component.ts
+++ chapter01/start_here/ng-dynamic-components/src/app/components/social-card/social-card.component.ts
@@ -1,17 +1,52 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { TwitterCardComponent } from './../twitter-card/twitter-card.component';
+import { FbCardComponent } from './../fb-card/fb-card.component';
+import {
+  Component,
+  OnInit,
+  Input,
+  ComponentFactoryResolver,
+  ViewChild,
+  ViewContainerRef,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { SocialCardType } from 'src/app/constants/social-card-type';
 
 @Component({
   selector: 'app-social-card',
   templateUrl: './social-card.component.html',
-  styleUrls: ['./social-card.component.scss']
+  styleUrls: ['./social-card.component.scss'],
 })
-export class SocialCardComponent implements OnInit {
+export class SocialCardComponent implements OnInit, OnChanges {
   @Input() type: SocialCardType;
-  cardTypes = SocialCardType;
-  constructor() { }
 
-  ngOnInit(): void {
+  @ViewChild('vfr', { read: ViewContainerRef }) vfr: ViewContainerRef;
+
+  cardTypes = SocialCardType;
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.type.currentValue !== undefined) {
+      console.log(`Card type changed to: ${changes.type.currentValue}`);
+      this.loadDynamicComponent(changes.type.currentValue);
+    }
   }
 
+  loadDynamicComponent(type: SocialCardType) {
+    let component;
+    switch (type) {
+      case SocialCardType.Facebook:
+        component = FbCardComponent;
+        break;
+      case SocialCardType.Twitter:
+        component = TwitterCardComponent;
+        break;
+    }
+    const componentFactory =
+      this.componentFactoryResolver.resolveComponentFactory(component);
+    this.vfr.clear();
+    this.vfr.createComponent(componentFactory);
+  }
 }
--- chapter02/start_here/ad-attribute-directive/src/app/app.component.ts
+++ chapter02/start_here/ad-attribute-directive/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ad-attribute-directive';
+  searchText = '';
 }
