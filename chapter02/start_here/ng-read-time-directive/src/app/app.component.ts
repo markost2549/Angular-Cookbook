b74d82250a261760e552f565c5446f009f34722d
Marko Stojkovic <markost2549@gmail.com>
2022-10-04 19:35:23 +0200

    added readTime directive

--- chapter02/start_here/ng-read-time-directive/src/app/app.component.ts
+++ chapter02/start_here/ng-read-time-directive/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  readTime: string;
+
+  onReadTimeCalculated(readTimeString: string) {
+    this.readTime = readTimeString;
+  }
 }
